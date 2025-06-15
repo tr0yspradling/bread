4df5478cabbb5d2fa445fb9fa71dd7da48c64504
Troy Spradling <ts@tr0y.me>
2025-06-14 22:39:06 -0400

    Fix template aliases in expected compatibility

--- include/expected_compat.h
+++ include/expected_compat.h
@@ -1,11 +1,15 @@
 #if __has_include(<expected>)
-#include <expected>
+#  include <expected>
 #else
-#include "../dependencies/tl_expected.hpp"
+#  include "../dependencies/tl_expected.hpp"
 namespace std {
-    using tl::expected;
-    using tl::unexpected;
-    template <class E>
-    using bad_expected_access = tl::bad_expected_access<E>;
+template <class T, class E>
+using expected = tl::expected<T, E>;
+
+template <class E>
+using unexpected = tl::unexpected<E>;
+
+template <class E>
+using bad_expected_access = tl::bad_expected_access<E>;
 }
 #endif
