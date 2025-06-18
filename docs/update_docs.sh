0ce313e5ba7dae918fdd7563eb834a22e232153c
Troy Spradling <ts@tr0y.me>
2025-06-18 01:48:07 -0400

    fix(docs): added venv

--- docs/update_docs.sh
+++ docs/update_docs.sh
@@ -3,5 +3,9 @@
 set -e
 
 # Build HTML documentation
+python3 -m venv venv
+source venv/bin/activate
+python3 -m pip install -r docs/requirements.txt
+
 cd "$(dirname "$0")"
 python3 -m sphinx -b html . _build
